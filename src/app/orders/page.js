85f1b5176d2cd8baacd3ba6a325aa01faf6e1448
Lazar Anca <ancalazar333@gmail.com>
2023-12-31 17:28:22 +0200

    update orders

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -36,15 +36,15 @@ export default function OrdersPage() {
                     <div
                         key={order._id}
                         className="bg-gray-100 mb-2 p-4 rounded-lg flex flex-col md:flex-row items-center gap-6">
-                        <div className="grow flex flex-col md:flex-row items-center gap-6">
-                            <div>
+                        <div className="grow flex flex-col md:flex-row items-center">
+                            {/* <div>
                                 <div className={
-                                    (order.paid ? 'bg-green-500' : 'bg-red-400')
-                                    + ' p-2 rounded-md text-white w-24 text-center'
+                                    //(order.paid ? 'bg-green-500' : 'bg-red-400')
+                                     ' p-2 rounded-md text-white w-24 text-center'
                                 }>
-                                    {order.paid ? 'Paid' : 'Not paid'}
+                                    
                                 </div>
-                            </div>
+                            </div> */}
                             <div className="grow">
                                 <div className="flex gap-2 items-center mb-1">
                                     <div className="grow">{order.userEmail}</div>
