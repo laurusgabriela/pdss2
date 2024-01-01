47973806abec5bd58aacd71472a2c09a3e92d823
Lazar Anca <ancalazar333@gmail.com>
2024-01-01 13:32:40 +0200

    edit

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -7,14 +7,7 @@ import Step3 from '../services/Step3';
 export default function HomeMenu(){
     return(
         <section className="">
-            <div className="absolute left-0 right-0 w-full" >
-                <div className="h-48 w-48 absolute -top-28 -left-12 -z-10">
-                    <Image src={'/salad1.png'} layout={'fill'} objectFit={'contain'} alt={''} />
-                </div>
-                <div className="h-48 w-48 absolute -top-28 -right-12 -z10">
-                    <Image src={'/salad2.png'} layout={'fill'} objectFit={'contain'} alt={''} />
-                </div>
-            </div>
+            
             <div className="text-center mb-4 ">
                 <SectionHeaders mainHeader={'Services'} />
             </div>
